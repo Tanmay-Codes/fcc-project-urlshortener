cc92c38df63322d55b749869108a13f51eebce5d
Tanmay Agrawal <tanmayagcode@gmail.com>
2024-06-17 21:27:59 +0530

    fix: api endpoint to redirect url

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.post("/api/shorturl", function (req, res) {
     }
   });
 });
-app.get("/api/:url", function (req, res) {
+app.get("/api/shorturl/:url", function (req, res) {
   const url = req.params.url;
   const redirectUrl = urlData.filter((items) => items.shortUrl == url)[0]
     .longUrl;
