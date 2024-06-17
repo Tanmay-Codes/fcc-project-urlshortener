6e4d7c9705a5034eca1161640f5e070916a2585d
Tanmay Agrawal <tanmayagcode@gmail.com>
2024-06-17 21:21:32 +0530

    fix : css and page layout

--- index.js
+++ index.js
@@ -8,11 +8,14 @@ const { urlencoded } = require("body-parser");
 // Basic Configuration
 const port = process.env.PORT || 3000;
 let urlData = [];
-app.use("/public", express.static(`${process.cwd()}/public`));
+// app.use("/public", express.static(`${process.cwd()}/public`));
+app.use(express.static(__dirname + "/public/"));
+
 app.use(cors());
 app.use(urlencoded({ extended: true }));
 app.get("/", function (req, res) {
-  res.sendFile(process.cwd() + "/views/index.html");
+  // res.sendFile(process.cwd() + "/views/index.html");
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 // Your first API endpoint
